7da4bead5fc2d2053726b0199a04b7494bfac7e1
d887892 <howie.kuo@team.telstra.com>
2026-09-18 12:49:04 +1200

    add activeDRB and ScheduledTTI to the per second trace

--- lte_ex2.cc
+++ lte_ex2.cc
@@ -3,6 +3,7 @@
  * 
  * extending ex1 
  *   - add per second data volumn output to the simulator
+ *   - add activeDRB and ScheduledTTI to the per second trace
  */
 
 #include "ns3/buildings-helper.h"
@@ -13,9 +14,26 @@
 
 #include <iomanip>
 #include <iostream>
+#include <set>
 #include <vector>
 
 using namespace ns3;
+
+static void
+CountDlScheduledTti(std::vector<std::set<uint64_t>>* dlScheduledTtiBySecond,
+                    uint32_t maxSecond,
+                    std::string,
+                    DlSchedulingCallbackInfo dlInfo)
+{
+	double nowSeconds = Simulator::Now().GetSeconds();
+	uint32_t intervalSec = static_cast<uint32_t>(nowSeconds) + 1;
+	if (intervalSec >= 1 && intervalSec <= maxSecond)
+	{
+		uint64_t ttiId = static_cast<uint64_t>(dlInfo.frameNo) * 10 +
+		                 static_cast<uint64_t>(dlInfo.subframeNo);
+		(*dlScheduledTtiBySecond)[intervalSec].insert(ttiId);
+	}
+}
 
 int
 main(int argc, char* argv[])
@@ -64,27 +82,46 @@ main(int argc, char* argv[])
 
 	const uint8_t firstDataLcid = 3;
 	const uint8_t lastDataLcid = 10;
+	const uint32_t numDataLcids = lastDataLcid - firstDataLcid + 1;
 	const uint32_t sampleSeconds = static_cast<uint32_t>(simTime.GetSeconds());
 	std::vector<uint64_t> dlCumulative(sampleSeconds + 1, 0);
 	std::vector<uint64_t> ulCumulative(sampleSeconds + 1, 0);
+	std::vector<double> dlAvgActiveDrb(sampleSeconds + 1, 0.0);
+	std::vector<uint64_t> prevDlRxBytes(ueDevs.GetN() * numDataLcids, 0);
+	std::vector<std::set<uint64_t>> dlScheduledTtiBySecond(sampleSeconds + 1);
+
+	Config::Connect("/NodeList/*/DeviceList/*/ComponentCarrierMap/*/LteEnbMac/DlScheduling",
+	                MakeBoundCallback(&CountDlScheduledTti, &dlScheduledTtiBySecond, sampleSeconds));
 
 	for (uint32_t sec = 1; sec <= sampleSeconds; ++sec)
 	{
 		Simulator::Schedule(Seconds(sec), [&, sec]() {
 			uint64_t dlBytes = 0;
 			uint64_t ulBytes = 0;
+			uint32_t activeDrbCount = 0;
 			for (uint32_t i = 0; i < ueDevs.GetN(); ++i)
 			{
 				Ptr<LteUeNetDevice> ueDev = ueDevs.Get(i)->GetObject<LteUeNetDevice>();
 				uint64_t imsi = ueDev->GetImsi();
+				uint32_t activeDrbForUe = 0;
 				for (uint8_t lcid = firstDataLcid; lcid <= lastDataLcid; ++lcid)
 				{
 					dlBytes += rlcStats->GetDlRxData(imsi, lcid);
 					ulBytes += rlcStats->GetUlRxData(imsi, lcid);
+
+					uint64_t currDlRxBytes = rlcStats->GetDlRxData(imsi, lcid);
+					size_t flowIndex = i * numDataLcids + (lcid - firstDataLcid);
+					if (currDlRxBytes > prevDlRxBytes[flowIndex])
+					{
+						++activeDrbForUe;
+					}
+					prevDlRxBytes[flowIndex] = currDlRxBytes;
 				}
+				activeDrbCount += activeDrbForUe;
 			}
 			dlCumulative[sec] = dlBytes;
 			ulCumulative[sec] = ulBytes;
+			dlAvgActiveDrb[sec] = static_cast<double>(activeDrbCount) / ueDevs.GetN();
 		});
 	}
 
@@ -98,20 +135,24 @@ main(int argc, char* argv[])
 	uint64_t simulatedTti = static_cast<uint64_t>(simulatedTime.GetMilliSeconds());
 
 	std::cout << "\n===== LTE Simulation Summary =====\n";
-	std::cout << std::left << std::setw(10) << "Time(s)" << std::setw(22)
-	          << "Downlink(MB,1s)" << std::setw(22) << "Uplink(MB,1s)" << "\n";
-	std::cout << "------------------------------------------------------\n";
+	std::cout << std::left << std::setw(10) << "Time(s)" << std::setw(20) << "Downlink(MB,1s)"
+	          << std::setw(18) << "Uplink(MB,1s)" << std::setw(20) << "DL Avg Active DRB"
+	          << std::setw(18) << "Scheduled TTI" << "\n";
+	std::cout << "--------------------------------------------------------------------------------\n";
 	for (uint32_t sec = 1; sec <= sampleSeconds; ++sec)
 	{
 		double dlIntervalMb = static_cast<double>(dlCumulative[sec] - dlCumulative[sec - 1]) /
 		                     (1024.0 * 1024.0);
 		double ulIntervalMb = static_cast<double>(ulCumulative[sec] - ulCumulative[sec - 1]) /
 		                     (1024.0 * 1024.0);
+		uint32_t scheduledTtiCount = dlScheduledTtiBySecond[sec].size();
 		std::cout << std::left << std::setw(10) << sec << std::fixed << std::setprecision(3)
-		          << std::setw(22) << dlIntervalMb << std::setw(22) << ulIntervalMb << "\n";
+		          << std::setw(20) << dlIntervalMb << std::setw(18) << ulIntervalMb
+		          << std::setw(20) << dlAvgActiveDrb[sec] << std::setw(18) << scheduledTtiCount
+		          << "\n";
 	}
 
-	std::cout << "------------------------------------------------------\n";
+	std::cout << "--------------------------------------------------------------------------------\n";
 	std::cout << std::fixed << std::setprecision(3)
 	          << "Total downlinked data: "
 	          << static_cast<double>(totalDlBytes) / (1024.0 * 1024.0) << " Megabytes\n";
